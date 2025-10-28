7d8e6c324e1b883bdb585bfd7daa287c69086f30
IhorSukhachov <i.sukhachov@gmail.com>
2025-10-28 09:01:49 +0200

    first swift ui project, just testing some basic features

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry", "Ron", "Hermi", "Max"]
+    @State private var tapCount: Int = 0
+    @State private var name: String = ""
+    @State private var selectedStudent = "Ron"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form {
+                Picker("Select a student", selection: $selectedStudent) {
+                    ForEach(students, id:\.self) {
+                        Text($0)
+                    }
+                }
+                
+            }.navigationTitle("Select a student")
+            
         }
-        .padding()
     }
 }
+      /*  NavigationStack {
+            
+                Form {
+                    Section {
+                        Text("Hello World")
+                        Text("Hello World")
+                        Text("Hello World")
+                    }
+                    Section {
+                        Text("Hello World")
+                        Text("Hello World")
+                        Text("Hello World")
+                    }
+            
+                }.navigationTitle("SwiftUI").navigationBarTitleDisplayMode(.inline)
+            
+        } */
+       // Button("Tap count: \(tapCount)") {
+         //   tapCount += 1
+        //}.buttonStyle(.glass)
+   /*     Form {
+            ForEach(1..<10) {number in
+                    Text("\(number)")
+                
+                }
+                
+            }
+        }
+       
 
+    }
+
+ */
 #Preview {
     ContentView()
 }
