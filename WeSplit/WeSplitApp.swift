2e5e1f19679a3315371a0729ca094b7f1789cc64
IhorSukhachov <i.sukhachov@gmail.com>
2025-10-28 07:41:13 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ihor Sukhachov on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
