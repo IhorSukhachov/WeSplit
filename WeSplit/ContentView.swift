40d9b5d446e2ada5388f7f677c2bbe6e046da2a6
IhorSukhachov <i.sukhachov@gmail.com>
2025-10-29 13:22:28 +0200

    added a picker for number of people

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,20 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
     
     let tipPercentages = [10, 15, 20, 25, 0]
     var body: some View {
+        NavigationStack {
         Form {
             Section {
                 TextField("Amount", value: $checkAmmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     .keyboardType(.decimalPad)
+                Picker("Number of people", selection: $numberOfPeople){
+                    ForEach(2..<100) {
+                        Text("\($0) people")
+                    }
+                }
+                .pickerStyle(.navigationLink)
             }
             Section {
                 Text(checkAmmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
             }
+        }  .navigationTitle("WeSplit")
         }
+      
+        
     }
 }
 
